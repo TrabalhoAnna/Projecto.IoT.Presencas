592c6736b4d3db3e6a9e85df9dce01f68622f4e4
Anna Almada <aalmada@mainhub.pt>
2022-12-13 17:31:48 +0000

    header

---
+++ Projecto.IoT.Presencas/assets/js/scriptAnna.js
@@ -0,0 +1,17 @@
+﻿function Editar() {
+    document.getElementById("btncancelaruser").style.display = "block";
+    document.getElementById("btnsalvaruser").style.display = "block";
+    document.getElementById("editaruser").style.display = "none";
+}
+
+function SalvarEdicao() {
+    document.getElementById("btncancelaruser").style.display = "none";
+    document.getElementById("btnsalvaruser").style.display = "none";
+    document.getElementById("editaruser").style.display = "block";
+}
+
+function CancelarEdicao() {
+    document.getElementById("btncancelaruser").style.display = "none";
+    document.getElementById("btnsalvaruser").style.display = "none";
+    document.getElementById("editaruser").style.display = "block";
+}
