1e8a380bdb9aacf56dbf10895ed99dd173506a5d
Pedro <pedrolico18_@hotmail.com>
2022-12-18 17:42:11 +0000

    A

--- Projecto.IoT.Presencas/assets/js/scriptAnna.js
+++ Projecto.IoT.Presencas/assets/js/scriptAnna.js
@@ -132,39 +132,6 @@ function Dispselect(Select) {
     dispSelecAnterior = Select.textContent;//Salva o conteudo do elemento selecionado
 }
 
-//apresentação da disciplina seleciona no Aluno
-var CursoSelecAnterior;//Para guardar qual a disciplina guardade anteriormente
-function Cursoselect(Select) {
-
-    var Curso = document.querySelectorAll('[id=Curso]');//Vai buscar todos os elementos com o id=Disciplina
-    var CursoS = document.getElementsByClassName('DispSelect');//Vai buscar todos os elementos com o class=DispSelect
-    var CursoTab = document.getElementById(Select.textContent);//Pega o elemento com o id = ao conteudo do elemento clicado
-    var CursoTabAnterior = document.getElementById(CursoSelecAnterior);//Pega o elemento com o id = ao conteudo do elemento clicado anteriormente
-
-    if (CursoS.length === 0) {//Caso nao exista um elemento com o id=DispSelect altera a classe do elemente clicado para DispSelect e mostra um novo card
-        Select.className = "DispSelect";
-        CursoTab.style.display = "block";
-    }
-    else {//Caso já exista
-        CursoS[0].className = "Disp";//Altera a classe do elemento que contenha como classe=DispSelect
-        CursoTabAnterior.style.display = "none";//Escondendo o card
-
-        for (var i = 0; i < Curso.length; i++) {//Percorre todos os elementos que tenham como id=Disciplina
-            if (Curso[i] === Select) {//Caso o elemento Selecionado for o mesmo que o elemento a ser percorrido
-                Curso[i].className = "DispSelect";//Altera a Classe desse elemento para DispSelect
-                CursoTab.style.display = "block";//Mostra o card
-            }
-            else {//Caso o elemento Selecionado não seja o mesmo que o elemento a ser percorrido
-                Curso[i].className = "Disp";//Mantem a classe Disp
-            }
-
-        }
-
-    }
-
-    CursoSelecAnterior = Select.textContent;//Salva o conteudo do elemento selecionado
-}
-
 var TrmTabAnterior;//Para guardar qual a disciplina guardade anteriormente
 
 function TrmSelect(Select) {
@@ -207,16 +174,8 @@ function TrmSelect(Select) {
 
 function cursos() {//Cria cards para cada Curso que haja
     var disp = document.querySelectorAll('[id=Disciplina]');//Vai buscar todos os elementos com o id=Disciplina
-    var cardT = document.getElementById("cardTabelas");
-    for (var i = 0; i < disp.length; i++) {
-        //Percorre todos os elementos que tenham como id=Disciplina
-        cardT.innerHTML += '<div id="' + disp[i].textContent + '" class="col-xl-7" style="display:none;">< !--begin::Tables Widget 9-- ><div class="card card-xl-stretch mb-5 mb-xl-8"><!--begin::Header--><div class="card-header border-0 pt-5"><h3 class="card-title align-items-start flex-column"><span class="card-label fw-bolder fs-3 mb-1">Sistema de Gestão Base de Dados</span></h3></div><!--end::Header--><!--begin::Body--><div class="card-body py-3"><!--begin::Table container--><div class="table-responsive"><!--begin::Table--><table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4"><!--begin::Table head--><thead><tr class="fw-bolder text-muted"><th class="min-w-200px">Data</th><th class="min-w-150px">Presença</th><th class="min-w-100px text-end">Justificação</th></tr></thead><!--end::Table head--><!--begin::Table body--><tbody><tr><td><div class="d-flex align-items-center"><div class="d-flex justify-content-start flex-column"><a class="text-dark fw-bolder text-hover-primary fs-6">12/12/2022</a></div></div></td><td><span class="badge badge-light-success fs-7 fw-bolder">PRESENTE</span></td><td></tr><tr><td><div class="d-flex align-items-center"><div class="d-flex justify-content-start flex-column"><span class="text-muted fw-bold text-muted d-block fs-7">12/12/2022</span></div></div></td><td><span class="badge badge-light-danger fs-7 fw-bolder">FALTA</span></td><td><div class="d-flex justify-content-end flex-shrink-0"><a class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" onclick="btnM(this, this.parentElement.parentElement.parentElement.children[0].children[0].children[0].children[0].textContent, this.parentElement.parentElement.parentElement, this.parentElement.children[1])"><!--begin::Svg Icon | path: icons/duotune/art/art005.svg--><span class="svg-icon svg-icon-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path opacity="0.3" d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z" fill="currentColor"></path><path d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z" fill="currentColor"></path></svg></span><!--end::Svg Icon--><!--lhglabgeaglkjabglkjagk.jg--></a><a style="display:none;" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" onclick="btnJustificado(this, this.parentElement.parentElement.parentElement.children[0].children[0].children[0].children[0].textContent, this.parentElement.parentElement.parentElement.children[3])"><i class="fonticon-chat fs-3"></i></a></div></td></tr><tr><td><div class="d-flex align-items-center"><div class="d-flex justify-content-start flex-column"><span class="text-muted fw-bold text-muted d-block fs-7">13/12/2022</span></div></div></td><td><span class="badge badge-light-danger fs-7 fw-bolder">FALTA</span></td><td><div class="d-flex justify-content-end flex-shrink-0"><a class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" onclick="btnM(this, this.parentElement.parentElement.parentElement.children[0].children[0].children[0].children[0].textContent, this.parentElement.parentElement.parentElement, this.parentElement.children[1])"><!--begin::Svg Icon | path: icons/duotune/art/art005.svg--><span class="svg-icon svg-icon-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path opacity="0.3" d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z" fill="currentColor"></path><path d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z" fill="currentColor"></path></svg></span><!--end::Svg Icon--></a><a style="display:none;" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1" onclick="btnJustificado(this, this.parentElement.parentElement.parentElement.children[0].children[0].children[0].children[0].textContent, this.parentElement.parentElement.parentElement.children[3])"><i class="fonticon-chat fs-3"></i></a></div></td></tr></tbody><!--end::Table body--></table><!--end::Table--></div><!--end::Table container--></div><!--begin::Body--></div><!--end::Tables Widget 9-- ></div > ';
-
-    }
-    var curso = document.querySelectorAll('[id=Curso]');//Vai buscar todos os elementos com o id=Disciplina
     var card = document.getElementById("CardsSelect");
-    alert(curso.length)
-    for (var i = 0; i < curso.length; i++) {
+    for (var i = 0; i < disp.length; i++) {
         //Percorre todos os elementos que tenham como id=Disciplina
         card.innerHTML += '<div id="' + disp[i].textContent + '" class="col-xl-5" style="display:none;"><!--begin::List Widget 6--><div class="card card-xl-stretch mb-xl-8"><!--begin::Header--><div class="card-header border-0 pt-5"><h3 class="card-title align-items-start flex-column"><span class="card-label fw-bolder text-dark">' + disp[i].textContent + '--> Turmas</span></h3></div><!--end::Header--><!--begin::Body--><div class="card-body pt-6"><!--begin::Item--><div class="d-flex flex-stack"><!--begin::Section--><div class="d-flex align-items-center flex-row-fluid flex-wrap"><!--begin:Author--><div class="flex-grow-1 me-2"><a id="Turma" class="Disp text-muted" onclick="TrmSelect(this)">PWDAM 1º Ano(' + (i + 1) + ')</a></div><!--end:Author--><!--begin::Actions--><a href="#" class="btn btn-sm btn-icon btn-bg-light btn-active-color-primary w-30px h-30px"><!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg--><span class="svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor"></rect><path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor"></path></svg></span><!--end::Svg Icon--></a><!--begin::Actions--></div><!--end::Section--></div><!--end::Item--><!--begin::Separator--><div class="separator separator-dashed my-4"></div><!--end::Separator--><!--begin::Item--><div class="d-flex flex-stack "><!--begin::Section--><div class="d-flex align-items-center flex-row-fluid flex-wrap"><!--begin:Author--><div class="flex-grow-1 me-2"><a id="Turma" class="Disp text-muted" onclick="TrmSelect(this)">PWDAM 2º Ano(' + (i + 1) +')</a></div><!--end:Author--><!--begin::Actions--><a href="#" class="btn btn-sm btn-icon btn-bg-light btn-active-color-primary w-30px h-30px"><!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg--><span class="svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor"></rect><path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor"></path></svg></span><!--end::Svg Icon--></a><!--begin::Actions--></div><!--end::Section--></div><!--end::Item--><!--begin::Separator--><div class="separator separator-dashed my-4"></div><!--end::Separator--></div><!--end::Body--></div><!--end::List Widget 6-- ></div > ';
         
